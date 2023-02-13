a21019a331c4d54c4a658473a54d47c28f79e900
Agustin Bustos Piasentini <bustos-agustin@hotmail.com - agustin.bustos.p@gmail.com>
2023-02-12 21:31:37 -0300

    Fix

--- Consolidar libros 2022.py
+++ Consolidar libros 2022.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 #import openpyxl
+from tkinter.filedialog import askdirectory
 
 #calcular el tiempo de ejecución del script
 import time
@@ -11,7 +12,7 @@ inicio = time.time()
 #leer el archivo Formato.xlsx
 Comprobante_C = pd.read_excel('Formato.xlsx', sheet_name='Comprobante_C')
 Alicuota_C = pd.read_excel('Formato.xlsx', sheet_name='Alicuota_C')
-Comprovante_V = pd.read_excel('Formato.xlsx', sheet_name='Comprobante_V')
+Comprobante_V = pd.read_excel('Formato.xlsx', sheet_name='Comprobante_V')
 Alicuota_V = pd.read_excel('Formato.xlsx', sheet_name='Alicuota_V')
 
 #leer todos lor achivos .txt no vacíos de la carpeta Consolidar
@@ -21,9 +22,9 @@ Alicuota_V = pd.read_excel('Formato.xlsx', sheet_name='Alicuota_V')
 # del Archivos
 
 #leer todos los .txt no vacíos de la carpeta 2022 y sus subcarpetas
-path = '2022'
+path = askdirectory(title='Seleccionar carpeta y subcarpetas a consolidar')
 Archivos_txt =  [os.path.join(dp, f) for dp, dn, filenames  in os.walk  (path) for  f in filenames  if (os.stat(os.path.join(dp, f)).st_size != 0 and f.endswith(".txt"))] 
-del path
+#del path
 
 #crear una nueva variable con los Archivos_txt que contengan la palabra 'Alicuota'
 Alicuota_txt = [i for i in Archivos_txt if 'Alicuota' in i]
@@ -41,13 +42,13 @@ del Archivos_txt
 #convertir las Columna 'Descripcion' de los dataframes Comprobante y Alicuota en listas
 Comprobante_desc_C = Comprobante_C['Descripcion'].tolist()
 Alicuota_desc_C = Alicuota_C['Descripcion'].tolist()
-Comprobante_desc_V = Comprovante_V['Descripcion'].tolist()
+Comprobante_desc_V = Comprobante_V['Descripcion'].tolist()
 Alicuota_desc_V = Alicuota_V['Descripcion'].tolist()
 
 #convertir las Columna 'Ancho' de los dataframes Comprobante y Alicuota en listas
 Comprobante_C = Comprobante_C['Ancho'].tolist()
 Alicuota_C = Alicuota_C['Ancho'].tolist()
-Comprobante_V = Comprovante_V['Ancho'].tolist()
+Comprobante_V = Comprobante_V['Ancho'].tolist()
 Alicuota_V = Alicuota_V['Ancho'].tolist()
 
 #crear un dataframe vacio para conslidar
@@ -123,11 +124,19 @@ for i in Alicuota_txt_V:
     ALIC = pd.read_fwf(i, widths=Alicuota_V, header=None , encoding=("latin1") , names = Alicuota_desc_V)
     ALIC['Archivo'] = i
     Consolidado_ALIC_V = pd.concat([Consolidado_ALIC_V, ALIC], axis=0)
-del Alicuota_desc_V, Alicuota_V, ALIC , Alicuota_txt_V
+del Alicuota_desc_V, Alicuota_V, ALIC , Alicuota_txt_V, i, Alicuota_txt, Comprobante_txt
 #Dividir las columnas 'Importe neto gravado' y 'Impuesto liquidado' por 100
 Consolidado_ALIC_V['Importe neto gravado'] = Consolidado_ALIC_V['Importe neto gravado']/100
 Consolidado_ALIC_V['Impuesto liquidado'] = Consolidado_ALIC_V['Impuesto liquidado']/100
 #Si el Tipo de comprobante es igual a (3 , 8 , 13 , 21 , 38 , 43 , 44 , 48 , 50 , 53 , 70 , 90 , 110 , 112 , 113 , 114 , 119 , 203 , 208 , 213) entonces multiplica el valor de las columnas 'Importe neto gravado' y 'Impuesto liquidado' por -1
+Consolidado_ALIC_V.loc[Consolidado_ALIC_V['Tipo de comprobante'].isin([3 , 8 , 13 , 21 , 38 , 43 , 44 , 48 , 50 , 53 , 70 , 90 , 110 , 112 , 113 , 114 , 119 , 203 , 208 , 213]) , ['Importe neto gravado' , 'Impuesto liquidado']] *= -1
+
+#Eliminar el Path de la columna ('Archivo' + '\') de todos los dataframe
+Consolidado_CBTE_V['Archivo'] = Consolidado_CBTE_V['Archivo'].str.replace(r'.*\\', '')
+Consolidado_ALIC_V['Archivo'] = Consolidado_ALIC_V['Archivo'].str.replace(r'.*\\', '')
+Consolidado_CBTE_C['Archivo'] = Consolidado_CBTE_C['Archivo'].str.replace(r'.*\\', '')
+Consolidado_ALIC_C['Archivo'] = Consolidado_ALIC_C['Archivo'].str.replace(r'.*\\', '')
+del path
 
 #crear Tablas dinamicas para todos los dataframe en base a la columnas 'Archivo' y 'Tipo de comprobante' 'Alícuota de IVA' y eliminar las columnas que no se necesitan
 
@@ -147,19 +156,30 @@ Consolidado_ALIC_VP = Consolidado_ALIC_V.pivot_table(index=['Archivo', 'Tipo de 
 #Eliminar columnas 'Número de comprobante' , 'Punto de venta'
 Consolidado_ALIC_VP = Consolidado_ALIC_VP.drop(['Número de comprobante', 'Punto de venta'], axis=1)
 
+#Eliminar los index de los dataframes
+Consolidado_CBTE_CP.reset_index(inplace=True)
+Consolidado_ALIC_CP.reset_index(inplace=True)
+Consolidado_CBTE_VP.reset_index(inplace=True)
+Consolidado_ALIC_VP.reset_index(inplace=True)
+
+#Exportar los dataframes de TD consolidados a un archivo excel
+Archivo_final = pd.ExcelWriter('Consolidado TD.xlsx', engine='openpyxl')
+Consolidado_CBTE_CP.to_excel(Archivo_final, sheet_name='CBTE_C TD' , index=False)
+Consolidado_ALIC_CP.to_excel(Archivo_final, sheet_name='ALIC_C TD' , index=False)
+Consolidado_CBTE_VP.to_excel(Archivo_final, sheet_name='CBTE_V TD' , index=False)
+Consolidado_ALIC_VP.to_excel(Archivo_final, sheet_name='ALIC_V TD' , index=False)
+Archivo_final.save()
+
 #Exportar los dataframes consolidados a un archivo excel
-Archivo_final = pd.ExcelWriter('Consolidado Anual.xlsx', engine='openpyxl')
-#Consolidado_CBTE_C.to_excel(Archivo_final, sheet_name='CBTE_C' , index=False)
-Consolidado_CBTE_CP.to_excel(Archivo_final, sheet_name='CBTE_C TD')
-#Consolidado_ALIC_C.to_excel(Archivo_final, sheet_name='ALIC_C' , index=False)
-Consolidado_ALIC_CP.to_excel(Archivo_final, sheet_name='ALIC_C TD')
-#Consolidado_CBTE_V.to_excel(Archivo_final, sheet_name='CBTE_V' , index=False)
-Consolidado_CBTE_VP.to_excel(Archivo_final, sheet_name='CBTE_V TD')
-#Consolidado_ALIC_V.to_excel(Archivo_final, sheet_name='ALIC_V' , index=False)
-Consolidado_ALIC_VP.to_excel(Archivo_final, sheet_name='ALIC_V TD')
+Archivo_final = pd.ExcelWriter('Consolidado.xlsx', engine='openpyxl')
+Consolidado_CBTE_C.to_excel(Archivo_final, sheet_name='CBTE_C' , index=False)
+Consolidado_ALIC_C.to_excel(Archivo_final, sheet_name='ALIC_C' , index=False)
+Consolidado_CBTE_V.to_excel(Archivo_final, sheet_name='CBTE_V' , index=False)
+Consolidado_ALIC_V.to_excel(Archivo_final, sheet_name='ALIC_V' , index=False)
 Archivo_final.save()
 
 #medir el tiempo de ejecución del programa en segundos y milisegundos
 fin = time.time()
 tiempo = fin - inicio
 print('Tiempo   :', tiempo, 'segundos')
+del tiempo, fin, inicio , Archivo_final
